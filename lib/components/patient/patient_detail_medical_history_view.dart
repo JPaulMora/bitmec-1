c831080f5450282df06bb75283dd631bc5429d34
Allan R. Garcia <0x.allan@gmail.com>
2019-12-06 11:41:49 -0600

    MedicalSurgeries Section added in PatientDetail Screen

--- lib/components/patient/patient_detail_medical_history_view.dart
+++ lib/components/patient/patient_detail_medical_history_view.dart
@@ -17,6 +17,7 @@ class _PatientDetailMedicalHistoryViewState
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           _MedicalConditionsSection(),
+          _MedicalSurgeriesSection(),
         ],
       ),
     );
@@ -58,6 +59,59 @@ class _MedicalConditionCard extends StatelessWidget {
                 Text('Diagnosticado port:'),
                 Text('Dr. Jose Pérez', style: TextStyle(
                   fontWeight: FontWeight.bold
+                )),
+              ],
+            ),
+
+            Row(
+              textDirection: TextDirection.rtl,
+              children: <Widget>[
+                Text('Fecha : Hora', style: TextStyle(
+                  color: Colors.blueAccent,
+                ))
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _MedicalSurgeriesSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListOfSection(
+      title: 'Cirugias',
+      children: <Widget>[
+        _MedicalSurgeryCard(),
+      ],
+    );
+  }
+}
+
+class _MedicalSurgeryCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 3.0,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+            horizontal: 10.0,
+            vertical: 10.0
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text('Cirugía', style: TextStyle(fontSize: 24.0)),
+                Padding(padding: const EdgeInsets.symmetric(vertical: 5.0)),
+                Text('Realizada por:'),
+                Text('Dr. Jose Pérez', style: TextStyle(
+                    fontWeight: FontWeight.bold
                 )),
               ],
             ),
