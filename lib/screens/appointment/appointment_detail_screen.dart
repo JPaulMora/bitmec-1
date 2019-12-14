c8f3d169eb7361c7d3ab60a9ffbecfdeb299bfaa
Allan R. Garcia <0x.allan@gmail.com>
2019-12-14 12:42:18 -0600

    Appoitment detaill fixed cards

--- lib/screens/appointment/appointment_detail_screen.dart
+++ lib/screens/appointment/appointment_detail_screen.dart
@@ -90,14 +90,18 @@ class _AppointmentDetailScreenState extends State<AppointmentDetailScreen> {
   Widget _buildVitalSignsSection(BuildContext context) {
     return ListOfSection(
       title: 'Signos Vitales',
-      children: <Widget>[],
+      children: <Widget>[
+        _VitalSignCard(),
+      ],
     );
   }
 
   Widget _buildSymptomsSection(BuildContext context) {
     return ListOfSection(
       title: 'Sintomas',
-      children: <Widget>[],
+      children: <Widget>[
+        _SymptomCard(),
+      ],
     );
   }
 
@@ -105,6 +109,114 @@ class _AppointmentDetailScreenState extends State<AppointmentDetailScreen> {
     return ListOfSection(
       title: 'Órdenes de Laboratorio',
       children: <Widget>[],
+    );
+  }
+}
+
+class _VitalSignCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 10.0, vertical: 10.0,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text('09 Diciembre 2019 - 7:20 pm', textAlign: TextAlign.end),
+              Padding(padding: const EdgeInsets.only(top: 10.0)),
+              Container(
+                color: Colors.black12,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10.0),
+                  child: Column(
+                    children: [
+                      _buildField('Peso', '25 libras'),
+                      _buildField('Altura', '12.0 metros'),
+                      _buildField('Sistólica', '4'),
+                      _buildField('Diastólica', '4'),
+                      _buildField('Frecuencia Cardiaca', '4ppm'),
+                      _buildField('Oxígeneo', '4%'),
+                      _buildField('Temperatura', '4.0 grados'),
+                      _buildField('Glucosa', '4 mg/dl'),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildField(String description, String value) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text('$description:'),
+          Text(value, style: TextStyle(
+            fontWeight: FontWeight.bold,
+          )),
+        ],
+      ),
+    );
+  }
+}
+
+class _SymptomCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 10.0, vertical: 10.0,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text('Ardor', style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.blue,
+              )),
+              Text('09 Diciembre 2019', style: TextStyle(
+                fontSize: 12.0,
+              )),
+
+              Padding(padding: const EdgeInsets.only(top: 10.0)),
+
+              Container(
+                color: Colors.black12,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 10.0,
+                    vertical: 10.0,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Text('Cabeza', style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      )),
+                      Text('Magnitud: 9')
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
